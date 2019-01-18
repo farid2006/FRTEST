ff358fd9e7ecca328c8b84a70445d7b816ac9733
farid <faridguessoumi@gmail.com>
2019-01-18 18:59:56 +0100

    n1

---
+++ entertainment.py
@@ -0,0 +1,12 @@
+import fresh_tomatoes
+import media
+
+Bloodsport=media.Movie("Chong Li vs Fighter","Frank Dux vs Pumola","https://i.ytimg.com/vi/OIC30sahflU/maxresdefault.jpg","https://www.youtube.com/watch?v=OIC30sahflU")
+BMovieRus=media.Movie("Van Damme vs Bolo Yeung","Van Damme","https://i.ytimg.com/vi/TNEf9XjGPFw/hqdefault.jpg","https://www.youtube.com/watch?v=OIC30sahflU")
+BMovieRus=media.Movie("Van Damme vs Bolo Yeung","Van Damme","https://i.ytimg.com/vi/TNEf9XjGPFw/hqdefault.jpg","https://www.youtube.com/watch?v=OIC30sahflU")
+BMovieRus=media.Movie("Van Damme vs Bolo Yeung","Van Damme","https://i.ytimg.com/vi/TNEf9XjGPFw/hqdefault.jpg","https://www.youtube.com/watch?v=OIC30sahflU")
+movies =[Bloodsport,BMovieRus,BMovieRus,BMovieRus]
+fresh_tomatoes.open_movies_page(movies)
+
+
+
